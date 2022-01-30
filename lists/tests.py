1f26b6068cb67b2bde79a4b16fa51a4b5cf32f04
AndreDutraDCC <cienciandre@gmail.com>
2022-01-30 01:20:24 -0300

    Implementando o Novo Projeto: Ajustando o Modelo

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -23,6 +23,7 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
 
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
@@ -35,16 +36,25 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
-class ItemModelTest(TestCase):
+
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        my_list = List()
+        my_list.save()
+
         first_item = Item()
         first_item.text = 'O primeiro item'
+        first_item.list = my_list
         first_item.save()
 
         second_item = Item()
         second_item.text = 'O segundo item'
+        second_item.list = my_list
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, my_list)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -52,4 +62,6 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(first_saved_item.list, my_list)
         self.assertEqual(second_saved_item.text, 'O segundo item')
+        self.assertEqual(second_saved_item.list, my_list)
